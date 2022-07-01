93da261f0984219a50a8dfae7c46fdafc0615284
JANE <m13221001019@163.com>
2022-07-01 14:28:59 +0800

    change avatar components (#17)
    * change avatar components
    
    * update vue version
    
    Co-authored-by: jane <yinyimingdodo@163.com>

--- src/components/AvatarSpace.vue
+++ src/components/AvatarSpace.vue
@@ -1,35 +1,30 @@
 <script setup lang="ts">
+import { sha256 } from 'js-sha256';
 import { computed } from 'vue';
-import { formatBytes32String } from '@ethersproject/strings';
-import { getUrl } from '@snapshot-labs/snapshot.js/src/utils';
-
-const props = defineProps<{
-  space: Record<string, any>;
-  size?: string;
-  symbolIndex?: string | number;
-}>();
-
-const spaceId = computed(() => props.space.id);
-
-const url = computed(() => {
-  const url = getUrl(props.space.avatar);
-  if (!url) return '';
-  return `https://worker.snapshot.org/mirror?img=${encodeURIComponent(url)}`;
+const props = withDefaults(
+  defineProps<{
+    space: { id: string; avatar?: string };
+    size?: string;
+    previewFile?: File;
+  }>(),
+  {
+    size: '22',
+    previewFile: undefined
+  }
+);
+const avatarHash = computed(() => {
+  if (!props.space?.avatar) return '';
+  const hash = sha256(props.space.avatar).slice(0, 16);
+  return `&cb=${hash}`;
 });
 
-const spaceAddress = computed(() => {
-  if (spaceId.value) return formatBytes32String(spaceId.value.slice(0, 24));
-  return '';
-});
+
 </script>
 
 <template>
-  <span class="inline-block align-middle leading-none">
-    <BaseAvatar
-      :space="space"
-      :imgsrc="url"
-      :address="spaceAddress"
-      :size="size"
-    />
-  </span>
+  <BaseAvatar
+    :preview-file="previewFile"
+    :size="size"
+    :src="`https://cdn.stamp.fyi/space/${space.id}?s=164`"
+  />
 </template>
